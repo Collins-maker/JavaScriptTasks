069a0b4bb75bbb8b1745356126b267c664ec4d41
Collins Muchui <muchuicollins56@gmail.com>
2023-05-11 11:54:07 +0300

    changes done in all the files

--- Date/date.js
+++ Date/date.js
@@ -4,6 +4,8 @@ let day= currentDate.getDate();
 let month=currentDate.getMonth() +1;
 let year=currentDate.getFullYear();
 
+//displaying dates using the format of day/momnth/year
+
 let orderedDate=day + "/" + month + "/" + year;
 
 console.log(orderedDate);
--- closestto100/closest100.js
+++ closestto100/closest100.js
@@ -1,17 +1,20 @@
-function closestTo100(a, b) {
-    // Calculate the difference between each value and 100
-    let diffA = Math.abs(a - 100);
-    let diffB = Math.abs(b - 100);
-  
-    // Compare the differences to find the closest value
-    if (diffA < diffB) {
-      return a;
-    } else {
-      return b;
-    }
-  }
-  let a=prompt("Enter the value of first number");
-  let b=prompt("Enter the value of second number");
+alert(
+  "we are comparing two numbers to see which one is closer to 100...sounds cool? press ok to give us the  two nos of your choice");
+function
+ closestTo100(a, b) {
+  // Calculate the difference between each value and 100
+  let diffX = Math.abs(x - 100);
+  let diffY = Math.abs(y - 100);
 
-  console.log(closes90tTo100(a,b));
-  
+  // Compare the differences to find the closest value
+  if (diffX < diffY) {
+    return x;
+  } else {
+    return y;
+  }
+}
+//let the user enter the two numbers separately and save them...
+let x = prompt("Enter the value of first number");
+let y = prompt("Enter the value of second number");
+
+console.log(closestTo100(x, y) + " is closer to 100");
